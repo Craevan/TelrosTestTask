abdd30a276cd76424ab8ddcb8f4c73d3c1929277
Crevan <a.siverskii@gmail.com>
2023-05-15 22:42:41 +0300

    [add] Web controller for admin

---
+++ src/main/java/com/crevan/telrostesttask/web/user/AdminController.java
@@ -0,0 +1,64 @@
+package com.crevan.telrostesttask.web.user;
+
+import com.crevan.telrostesttask.model.User;
+import com.crevan.telrostesttask.util.validation.ValidationUtil;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(value = AdminController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminController extends AbstractUserController {
+
+    static final String REST_URL = "/api/admin/users";
+
+    @GetMapping
+    public List<User> getAll() {
+        log.info("[AdminController:] Getting all users");
+        return repository.findAll();
+    }
+
+    @GetMapping("/by-email")
+    public User getByEmail(@RequestParam final String email) {
+        log.info("[AdminController:] Get user with email={}", email);
+        return repository.getExistedByEmail(email);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable final int id) {
+        log.info("[AdminController:] Get user with ID={}", id);
+        return repository.getExisted(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@Valid @RequestBody final User user) {
+        log.info("[AdminController:] Create {}", user);
+        ValidationUtil.checkNew(user);
+        ValidationUtil.checkRoles(user);
+        User created = repository.prepareAndSave(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@Valid @RequestBody final User user, @PathVariable final int id) {
+        log.info("[AdminController:] Updating {} with id={}", user, id);
+        ValidationUtil.assureIdConsistent(user, id);
+        repository.prepareAndSave(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable final int id) {
+        log.info("[AdminController:] Delete user with ID={}", id);
+        repository.deleteExisted(id);
+    }
+}
---
+++ src/main/java/com/crevan/telrostesttask/web/user/UniqueMailValidator.java
@@ -0,0 +1,50 @@
+package com.crevan.telrostesttask.web.user;
+
+import com.crevan.telrostesttask.HasId;
+import com.crevan.telrostesttask.repository.UserRepository;
+import com.crevan.telrostesttask.web.AuthUser;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@AllArgsConstructor
+public class UniqueMailValidator implements Validator {
+
+    public static final String EXCEPTION_DUPLICATE_EMAIL = "User with this email already exists";
+
+    private final UserRepository repository;
+    private final HttpServletRequest request;
+
+    @Override
+    public boolean supports(@NonNull Class<?> clazz) {
+        return HasId.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(@NonNull Object target, @NonNull Errors errors) {
+        HasId user = ((HasId) target);
+        if (StringUtils.hasText(user.getEmail())) {
+            repository.findByEmailIgnoreCase(user.getEmail())
+                    .ifPresent(dbUser -> {
+                        if (request.getMethod().equals("PUT")) {  // UPDATE
+                            int dbId = dbUser.id();
+
+                            // it is ok, if update ourselves
+                            if (user.getId() != null && dbId == user.id()) return;
+
+                            // Workaround for update with user.id=null in request body
+                            // ValidationUtil.assureIdConsistent called after this validation
+                            String requestURI = request.getRequestURI();
+                            if (requestURI.endsWith("/" + dbId) || (dbId == AuthUser.authId() && requestURI.contains("/profile")))
+                                return;
+                        }
+                        errors.rejectValue("email", "", EXCEPTION_DUPLICATE_EMAIL);
+                    });
+        }
+    }
+}
