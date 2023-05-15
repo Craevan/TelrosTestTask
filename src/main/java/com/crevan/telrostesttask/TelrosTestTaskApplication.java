41ba978e63a889b379a96391ff6a980f02aa3912
Crevan <a.siverskii@gmail.com>
2023-05-15 17:29:01 +0300

    init commit

---
+++ src/main/java/com/crevan/telrostesttask/TelrosTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.crevan.telrostesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelrosTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelrosTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crevan/telrostesttask/TelrosTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crevan.telrostesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelrosTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
