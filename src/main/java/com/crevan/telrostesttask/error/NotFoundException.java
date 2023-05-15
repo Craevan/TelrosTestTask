0bc8492c164666499fa43787ff8aa1b109895620
Crevan <a.siverskii@gmail.com>
2023-05-15 22:40:06 +0300

    [add] custom exceptions

---
+++ src/main/java/com/crevan/telrostesttask/error/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.crevan.telrostesttask.error;
+
+public class NotFoundException extends AppException {
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}
