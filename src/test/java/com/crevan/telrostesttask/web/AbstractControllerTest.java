1077412a945569cdc87f6ce949a3922a73d8f361
Crevan <a.siverskii@gmail.com>
2023-05-16 22:20:40 +0300

    [update] set @ActiveProfile

--- src/test/java/com/crevan/telrostesttask/web/AbstractControllerTest.java
+++ src/test/java/com/crevan/telrostesttask/web/AbstractControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 public abstract class AbstractControllerTest {
 
